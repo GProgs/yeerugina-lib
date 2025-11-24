a40ed60584c5094a7c437d35dd3ec9cda1da44b8
Gentrit Zenuni <gentrit.zenuni@gmail.com>
2025-11-24 02:47:18 +0200

    clean

--- src/lib.rs
+++ src/lib.rs
@@ -13,20 +13,3 @@ mod derive_alias {
         Eqs = ::std::cmp::PartialEq, ::std::cmp::Eq;
     }
 }
-
-/*
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-*/
