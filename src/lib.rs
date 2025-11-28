220c467a3265888c281f88c71d5f43a491338197
Gentrit Zenuni <gentrit.zenuni@gmail.com>
2025-11-28 03:07:11 +0200

    try to workaround unused_crate_dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 //! Library for controlling Yeelight lamps through Rust.
-use palette as _;
+
+// Annoying bug with the unused_crate_dependencies lint:
+// rustc doesn't see all uses of extern crates,
+// so stuff like "derive_more" flies under the radar,
+// and the linter cries wolf.
+// https://github.com/rust-lang/rust/issues/95513
+
+// This is a VERY hacky workaround.
+#![allow(unused_crate_dependencies)] // Allow in general
+#![cfg(not(debug_assertions))] // ... but if we're not debugging ...
+#![deny(unused_crate_dependencies)] // ... make sure we don't have any unused deps!
+mod _unused {} // limit the scope of the cfg(not(...))
 
 /// Modules for commands.
 pub mod cmd;
