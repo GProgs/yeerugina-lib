2482388a2c0c086c267cc30bfc7771534b68a5cd
Gentrit Zenuni <gentrit.zenuni@gmail.com>
2025-11-29 01:42:06 +0200

    new workaround for lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,13 @@
 //! Library for controlling Yeelight lamps through Rust.
 
-// Annoying bug with the unused_crate_dependencies lint:
-// rustc doesn't see all uses of extern crates,
-// so stuff like "derive_more" flies under the radar,
-// and the linter cries wolf.
-// https://github.com/rust-lang/rust/issues/95513
-
-// This is a VERY hacky workaround.
-#![allow(unused_crate_dependencies)] // Allow in general
-#![cfg(not(debug_assertions))] // ... but if we're not debugging ...
-#![deny(unused_crate_dependencies)] // ... make sure we don't have any unused deps!
-mod _unused {} // limit the scope of the cfg(not(...))
-
 /// Modules for commands.
 pub mod cmd;
 /// Module for code related to interfacing with lamps.
 pub mod lamp;
 
+/// Module containing aliases for derive macros.
+///
+/// This is used by the "derive_aliases" crate.
 mod derive_alias {
     derive_aliases::define! {
         Serde = ::serde::Serialize, ::serde::Deserialize;
@@ -24,13 +15,13 @@ mod derive_alias {
     }
 }
 
-/*
+// Add "unused" extern crates here.
+// Annoying bug with the unused_crate_dependencies lint:
+// rustc doesn't see all uses of extern crates,
+// so stuff like "derive_more" flies under the radar,
+// and the linter cries wolf.
+// https://github.com/rust-lang/rust/issues/95513
 
-extern crate proc_macro;
-use proc_macro::TokenStream;
-
-#[proc_macro]
-pub fn duration_ms(_item: TokenStream) -> TokenStream {
-    r#"#[serde_as(as = "DurationMilliSeconds<u64>")]"#.parse().unwrap()
-}
-*/
+// Put dev-dependencies emitting warns here.
+#[cfg(test)]
+use colog as _;
